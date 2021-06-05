a5e99abd8512d4c6bacbf2cd59f2176dad094b9f
Abhishekp12 <abhiec.2011@gmail.com>
2021-06-06 01:47:47 +0530

    Ui done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Simple from './Component/Simple';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Simple />
+       
+    </div>
+  );
+}
+
+export default App;
